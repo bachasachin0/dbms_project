d0fe0fd7a68981dfc279c676b941bd8656198f03
bachasachin0 <119314805+bachasachin0@users.noreply.github.com>
2024-03-16 00:51:20 +0530

    Web Application

---
+++ app.py
@@ -0,0 +1,144 @@
+import os
+import streamlit as st
+from sql import connect_to_db,check_db_connection,insert_person_data,dsn,insert_incident_data,insert_participation_data,execute_query,select_all_data,select_specific_columns,retrieve_suspects
+import pandas as pd
+
+def insertion_page():
+    # Connect to the database
+    conn = connect_to_db(dsn)
+ 
+    tab1, tab2, tab3 = st.tabs(["Add Person Data", "Add Incident Data", "Add Participation Data"])
+    # Page for adding data to Person table
+    with tab1:
+        st.header('Add Person Data')
+        name = st.text_input('Name:', key='name_input')
+        dob = st.date_input('Date of Birth:', key='dob_input')
+        ssn = st.text_input('Social Security Number:', key='ssn_input')
+        st_name = st.text_input('State:', key='st_name_input')
+        city = st.text_input('City:', key='city_input')
+        if st.button('Add Person Data'):
+            pid = insert_person_data(conn, name, dob, ssn, st_name, city)
+            st.success(f'Data added successfully to Person table. PID: {pid}')
+
+    # Page for adding data to Incident table
+    with tab2:
+        st.header('Add Incident Data')
+        crimetype_options = ['Theft', 'Assault', 'Burglary', 'Robbery', 'Vandalism', 'Fraud', 'Drug Possession', 'Homicide', 'Other']
+        crimetype = st.selectbox('Crime Type:', crimetype_options, key='crimetype_input')
+        date = st.date_input('Date of Incident:', key='date_input')
+        time = st.time_input('Time of Incident:', key='time_input')
+        formatted_datetime = f"{date} {time}:00"
+        st_name_incident = st.text_input('State:', key='st_name_incident_input')
+        city_incident = st.text_input('City:', key='city_incident_input')
+
+        if st.button('Add Incident Data'):
+            iid = insert_incident_data(conn, crimetype, formatted_datetime, st_name_incident, city_incident)
+            if iid:
+                st.success(f'Data added successfully to Incident table. IID: {iid}')
+            else:
+                st.error('Failed to add data to Incident table.')
+
+    # Page for adding data to Participation table
+    with tab3:
+        # Input fields for participation data
+        st.header("insert Particiaption data")
+        pid = st.text_input('Person ID (PID):', key='pid_input')
+        iid = st.text_input('Incident ID (IID):', key='iid_input')
+        role = st.selectbox('Role:', ['victim', 'witnessed', 'suspect'], key='role_input')
+
+        if st.button('Add Participation Data'):
+            success, message = insert_participation_data(conn, pid, iid, role)
+            if success:
+                st.success(message)
+            else:
+                st.error(message)
+
+def retrival_page():
+    # Connect to the database
+    conn = connect_to_db(dsn)
+ 
+    st.title('Database Query Tool')
+    st.write('Interface To Search data from database')
+     # Query options
+    query_type = st.selectbox('Select query type:', ['Select All Data', 'Select Specific Columns', 'Retrieve Suspects'])
+
+    # Execute selected query type
+    if query_type == 'Select All Data':
+        table_name = st.selectbox('Select table:', ['person', 'incident', 'participation'])
+        data = select_all_data(conn, table_name)
+        if data is not None:
+            st.write('Query Result:')
+            st.dataframe(data)
+        else:
+            st.error('No data found.')
+        
+            
+    elif query_type == 'Select Specific Columns':
+        table_name = st.selectbox('Select table:', ['person', 'incident', 'participation'])
+        
+        # Retrieve column names for the selected table
+        query = f"SELECT COLUMN_NAME FROM SYSIBM.COLUMNS WHERE TABLE_NAME = '{table_name.upper()}'"
+        column_names = [row['COLUMN_NAME'] for row in execute_query(conn, query)]
+        
+        selected_columns = st.multiselect('Select columns to retrieve:', column_names)
+        
+        if selected_columns:
+            data = select_specific_columns(conn, table_name, selected_columns)
+            if data:
+                df = pd.DataFrame(data)
+                st.write('Query Result:')
+                st.dataframe(df)
+            else:
+                st.warning('Please select at least one column.')
+
+    # elif query_type == 'Filter Data Based on Condition':
+    #     table_name = st.selectbox('Select table:', ['person', 'incident', 'participation'])
+    #     column_name = st.text_input('Enter column name to filter:', 'state')
+    #     filter_value = st.text_input('Enter filter value:', 'California')
+    #     data = filter_data(conn, table_name, column_name, filter_value)
+    #     if data:
+    #         st.write('Query Result:')
+    #         st.write(data)
+
+    elif query_type == 'Retrieve Suspects':
+        role = st.selectbox('Select role:', ['victim', 'witnessed', 'suspect'])
+        data = retrieve_suspects(conn, role)
+        if data:
+            df1=pd.DataFrame(data)
+            st.write('Query Result:')
+            st.write(df1)
+
+
+# Streamlit web application
+def main():
+    st.title('Database Interaction')
+    st.sidebar.image('img\logo2.jpg',use_column_width=True)
+    st.sidebar.title("Navigation Pane")
+    
+    page = st.sidebar.radio("Go to", ("Insert Data", "Retrieve Data"))
+    
+    # Connect to the database
+    conn = connect_to_db(dsn)
+ 
+    # Check database connection
+    if check_db_connection(conn):
+        st.success("Database connected successfully!")
+        if page == "Insert Data":
+            insertion_page()
+        elif page == "Retrieve Data":
+            retrival_page()
+    else:
+        st.error("Failed to connect to the database.")
+   
+
+
+
+
+
+
+    
+      
+    
+
+if __name__ == '__main__':
+    main()
--- sql.py
+++ sql.py
@@ -1,27 +1,195 @@
-import mysql.connector
-from dotenv import load_dotenv
+
 import os
+from dotenv import load_dotenv
 load_dotenv()
+import ibm_db
+import pandas as pd
+
+dsn_hostname=os.getenv("Host_name")
+dsn_uid= os.getenv("uid")
+dsn_pwd=os.getenv("password")
+dsn_port= os.getenv("Port_number")
+dsn_database=os.getenv("database")
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_protocol = "TCPIP" 
+dsn_security = "SSL"
 
 
-db = mysql.connector.connect(
-    host=os.getenv("Host"),
-    user=os.getenv("user"),
-    password=os.getenv("password"),
-    database=os.getenv("database")
-)
+dsn = (
+        "DRIVER={0};"
+        "DATABASE={1};"
+        "HOSTNAME={2};"
+        "PORT={3};"
+        "PROTOCOL={4};"
+        "UID={5};"
+        "PWD={6};"
+        "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
 
-cursor = db.cursor()
 
-cursor.execute("SELECT * FROM chicago_crime")
+try:
+        conn = ibm_db.connect(dsn, "", "")
+        print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+        # return conn
+except:
+        print ("Unable to connect: ", ibm_db.conn_errormsg() )
+        # return None
 
-# Fetch the results
-results = cursor.fetchall()
 
-# Process the results
-for row in results:
-    print(row)
+# to connect the database
+def connect_to_db(dsn):
+     conn=ibm_db.connect(dsn,"","")
+     return conn
 
-db.commit()
-cursor.close()
-db.close()
+# to check the database connection
+def check_db_connection(conn):
+    if conn:
+        # print("connected!")
+        return True
+    else:
+        return False
+    
+
+# # # Function to insert data into the Person table
+def insert_person_data(conn, name, dob, ssn, st_name, city):
+    sql_insert = "INSERT INTO Person (name, dob, ssn, st_name, city) VALUES (?, ?, ?, ?, ?)"
+    stmt_insert = ibm_db.prepare(conn, sql_insert)
+    if not stmt_insert:
+        return None, "Failed to prepare insert statement"
+    
+    ibm_db.bind_param(stmt_insert, 1, name)
+    ibm_db.bind_param(stmt_insert, 2, dob)
+    ibm_db.bind_param(stmt_insert, 3, ssn)
+    ibm_db.bind_param(stmt_insert, 4, st_name)
+    ibm_db.bind_param(stmt_insert, 5, city)
+    
+    result = ibm_db.execute(stmt_insert)
+    if result:
+        sql_last_id = "SELECT pid FROM Person ORDER BY pid DESC LIMIT 1"
+        stmt_last_id = ibm_db.exec_immediate(conn, sql_last_id)
+        last_pid = ibm_db.fetch_both(stmt_last_id)
+        if last_pid:
+            return last_pid[0], f"Person with PID {last_pid[0]} successfully registered."
+        else:
+            return None, "Failed to fetch last inserted PID"
+    else:
+        return None, "Failed to insert data into the database"
+    
+def insert_incident_data(conn, crimetype, time, st_name, city):
+    try:
+        sql = "INSERT INTO Incident (crimetype, time, st_name, city) VALUES (?, ?, ?, ?)"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, crimetype)
+        ibm_db.bind_param(stmt, 2, time)
+        ibm_db.bind_param(stmt, 3, st_name)
+        ibm_db.bind_param(stmt, 4, city)
+        ibm_db.execute(stmt)
+        
+        # Fetch the iid of the last inserted record
+        sql_last_iid = "SELECT iid FROM Incident ORDER BY iid DESC LIMIT 1"
+        stmt_last_iid = ibm_db.exec_immediate(conn, sql_last_iid)
+        last_iid = ibm_db.fetch_both(stmt_last_iid)
+        
+        if last_iid:
+            return last_iid[0]
+        else:
+            return None
+    
+    except Exception as e:
+        return False ,f"Failed to insert incident data: {e}"
+
+
+
+def insert_participation_data(conn, pid, iid, role):
+    try:
+        # Check if pid exists in Person table
+        sql_check_pid = f"SELECT pid FROM Person WHERE pid = ?"
+        stmt_check_pid = ibm_db.prepare(conn, sql_check_pid)
+        ibm_db.bind_param(stmt_check_pid, 1, pid)
+        ibm_db.execute(stmt_check_pid)
+        if not ibm_db.fetch_assoc(stmt_check_pid):
+            return False, f"PID {pid} does not exist in Person table"
+        
+        # Check if iid exists in Incident table
+        sql_check_iid = f"SELECT iid FROM Incident WHERE iid = ?"
+        stmt_check_iid = ibm_db.prepare(conn, sql_check_iid)
+        ibm_db.bind_param(stmt_check_iid, 1, iid)
+        ibm_db.execute(stmt_check_iid)
+        if not ibm_db.fetch_assoc(stmt_check_iid):
+            return False, f"IID {iid} does not exist in Incident table"
+        
+        # # Validate role
+        # valid_roles = ['Victim', 'Witness', 'Suspect']
+        # if role not in valid_roles:
+        #     return False, f"Role should be one of {', '.join(valid_roles)}"
+        
+        # Insert data into Participation table
+        sql_insert = "INSERT INTO Participation (pid, iid, role) VALUES (?, ?, ?)"
+        stmt_insert = ibm_db.prepare(conn, sql_insert)
+        ibm_db.bind_param(stmt_insert, 1, pid)
+        ibm_db.bind_param(stmt_insert, 2, iid)
+        ibm_db.bind_param(stmt_insert, 3, role)
+        ibm_db.execute(stmt_insert)
+        
+        return True, "Participation data inserted successfully"
+    
+    except Exception as e:
+        return False, f"Failed to insert participation data: {e}"
+
+    
+# Function to execute SQL query and return data
+def execute_query(conn, query, params=None):
+    try:
+        stmt = ibm_db.prepare(conn, query)
+        if params:
+            if isinstance(params, (list, tuple)):
+                ibm_db.execute(stmt, params)
+            else:
+                ibm_db.execute(stmt, (params,))
+        else:
+            ibm_db.execute(stmt)
+        
+        data = []
+        result = ibm_db.fetch_assoc(stmt)
+        while result:
+            data.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        
+        return data
+    except Exception as e:
+        return None, f"Error executing SQL query: {e}"
+        # return None
+        
+    
+# Function to select all data from a table
+def select_all_data(conn, table_name):
+    query = f"SELECT * FROM {table_name}"
+    return execute_query(conn, query)
+
+#Function to select specific columns of table
+def select_specific_columns(conn, table_name, selected_columns):
+    try:
+        # Construct the SQL query
+        columns_str = ', '.join(selected_columns)
+        query = f"SELECT {columns_str} FROM {table_name}"
+        
+        # Execute the query
+        data = execute_query(conn, query)
+        return data
+    except Exception as e:
+        return False, f"Error executing SQL query: {e}"
+        # return None
+
+
+
+# Function to retrieve suspects based on role
+def retrieve_suspects(conn, role):
+    query = """
+    SELECT p.name, i.time, i.st_name, i.city,pa.role
+    FROM person p
+    JOIN participation pa ON p.pid = pa.pid
+    JOIN incident i ON pa.iid = i.iid
+    WHERE pa.role = ?
+    """
+    params = (role,)
+    return execute_query(conn, query, params)
+
